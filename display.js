35b6dfdaa5457c210cda8b52c30d1a9da65f4721
kanalysis-drone <ukanahpetersunday@gmail.com>
2022-07-10 09:46:23 +0100

    first commit

---
+++ display.js
@@ -0,0 +1,9 @@
+alert("The following display my details");
+let user={
+	name: "Ukanah Peter Sunday",
+	height: "6ft",
+	country: "Nigeria",
+};
+alert("My name is " + user.name);
+alert("I am " + user.height + " tall");
+alert("I hail from " + user.country);
